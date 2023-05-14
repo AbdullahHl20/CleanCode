2aeffb0aa3d1e7068df8855b62c9ea35f1b057b8
ASUS <ASUS@ABDULLAH>
2023-05-14 22:05:36 +0300

    Add MAIN

--- CleanCode/Program.cs
+++ CleanCode/Program.cs
@@ -16,7 +16,7 @@ namespace CleanCode
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainDialog());
         }
     }
 }
